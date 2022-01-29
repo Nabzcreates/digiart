4b14f7c74f445e015b0657820e78a7f8acdbc3f6
Nabzcreates <88214757+Nabzcreates@users.noreply.github.com>
2022-01-29 12:50:08 -0800

    final commit

--- database/myapp/src/reducer.js
+++ database/myapp/src/reducer.js
@@ -1,49 +1,48 @@
 export const initialState = {
-  basket: [],
-  bid: 0,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
+    basket: [],
+    bid: 0,
+  };
+  
+  const reducer = (state, action) => {
+    switch (action.type) {
       case "ADD_TO_BASKET":
-          return {
-              ...state,
-              basket: [...state.basket, action.item],
-          };
-
+        return {
+          ...state,
+          basket: [...state.basket, action.item],
+        };
+  
       case "REMOVE_FROM_BASKET":
-          let newBasket = [...state.basket];
-          const index = state.basket.findIndex(
-              (item) => item.id === action.id
-          );
-
-          if (index >= 0) {
-              newBasket.splice(index, 1);
-          } else {
-              console.warn(`Cannot remove digital asset, id: ${action.id}`);
-          }
-
-          return {
-              ...state,
-              basket: newBasket,
-          };
-
+        let newBasket = [...state.basket];
+        const index = state.basket.findIndex((item) => item.id === action.id);
+  
+        if (index >= 0) {
+          newBasket.splice(index, 1);
+        } else {
+          console.warn(`Cannot remove digital asset, id: ${action.id}`);
+        }
+  
+        return {
+          ...state,
+          basket: newBasket,
+        };
+  
       case "CHANGE_QTY":
-          return {
-              ...state,
-              basket: state.basket.filter((item) =>
-                  item.id === action.id ? (item.qty = action.qty) : item.qty
-              ),
-          };
-
+        return {
+          ...state,
+          basket: state.basket.filter((item) =>
+            item.id === action.id ? (item.qty = action.qty) : item.qty
+          ),
+        };
+  
       case "BID":
-          return {
-              bid: action.item,
-          };
-
+        return {
+          ...state,
+          bid: action.item,
+        };
+  
       default:
-          return state;
-  }
-};
-
-export default reducer;
+        return state;
+    }
+  };
+  
+  export default reducer;
